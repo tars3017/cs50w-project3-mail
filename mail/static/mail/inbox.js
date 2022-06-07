545b62f1d1d9080fca0070af40458b2e3e132ef7
Chuang Bing Cheng <tars3017@debian-BULLSEYE-live-builder-AMD64>
2022-06-07 21:51:42 +0800

    First commit, ready to make archive/unarchive function.

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -14,6 +14,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#display-mails').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -42,47 +43,108 @@ function send_email(event) {
   load_mailbox('sent');
 }
 
+function archive_mail(mail_id) {
+  // TODO
+}
+
+function display_mail(mail_id) {
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#display-mails').style.display = 'block';
+
+  // mark as read
+  fetch(`/emails/${mail_id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+      read: true
+    })
+  });
+
+  // display the mail
+  fetch(`/emails/${mail_id}`, {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    let title = document.createElement('h3');
+    title.innerHTML = data.subject;
+
+    let sender = document.createElement('div');
+    sender.innerHTML = `<strong>${data.sender}</strong>`;
+
+    let body = document.createElement('div');
+    body.innerHTML = data.body;
+
+    let time = document.createElement('div');
+    time.innerHTML = data.timestamp;
+    time.setAttribute('style', 'float: right;');
+
+    let btn = document.createElement('button');
+    innerHTML = "TEST";
+    if (data.archived) {
+      btn.innerHTML = "Unarchive";
+    }
+    else {
+      btn.innerHTML = "Archive";
+    }
+    btn.addEventListener('click', () => archive_mail(mail_id));
+
+    const part = document.querySelector('#display-mails');
+    part.innerHTML = '';
+    part.appendChild(title);
+    part.appendChild(time);
+    part.appendChild(sender)
+    part.appendChild(body);
+    part.appendChild(btn);
+   
+  })
+  
+}
 
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#display-mails').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
-  if (mailbox === 'inbox') {
-    fetch('/emails/inbox', {
-      methond: 'GET'
-    })
-    .then(response => response.json())
-    .then(data => {
-      console.log(data);
-      data.forEach(mail => {
-        const sender = mail.sender;
-        const subject = mail.subject;
-        const timestamp = mail.timestamp;
-        const read = mail.read;
-
-        if (read) {
-          document.querySelector('#emails-view').setAttribute('style', 'color: #D0FCB3');
-          document.querySelector('#emails-view').innerHTML += `<div background="#D0FCB3">${sender} <strong>${subject}</strong> <span color="blue">at ${timestamp}</span></div>`;
-          
-        }
-        else {
-          document.querySelector('#emails-view').innerHTML += `<div background="#D0FCB3">${sender} <strong>${subject}</strong> <span color="blue">at ${timestamp}</span></div>`;
-        }
-      })
-      
+  fetch(`/emails/${mailbox}`, {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data);
+    data.forEach(mail => {
+      const sender = mail.sender;
+      const subject = mail.subject;
+      const timestamp = mail.timestamp;
+      const read = mail.read;
+      const id = mail.id;
+      if (read) {
+        let element = document.createElement('div');
+        element.innerHTML = `<div>${sender} send <strong>${subject}</strong> at <i>${timestamp}</i></div>`;
+        // element.style.color = "#D0FCB3";
+        element.setAttribute('style', 'background: #FEFEFE; margin: 5px; padding: 2px; border-radius: 5px; border-style: solid; border-width: 1px;');
+        element.addEventListener('click', () => display_mail(id));
+        document.querySelector('#emails-view').appendChild(element);
+        // document.querySelector('#emails-view').setAttribute('style', 'color: #D0FCB3');
+        // document.querySelector('#emails-view').innerHTML += 
+        
+      }
+      else {
+        let element = document.createElement('div');
+        element.innerHTML = `<div">${sender} send <strong>${subject}</strong> at <i>${timestamp}</i></div>`;
+        // element.style.color = "blue";
+        element.setAttribute('style', 'background: #C9CAD9; margin: 5px; padding: 2px; border-radius: 5px; border-style: solid; border-width: 1px;');
+        element.addEventListener('click', () => display_mail(id));
+        document.querySelector('#emails-view').appendChild(element);
+        // document.querySelector('.mails').style.color = "blue";
+      }
     });
-    const mails = document.createElement('div');
-
-  }
-  else if (mailbox === 'sent') {
-
-  } 
-  else { // archieved
-
-  }
+    
+  });
 }
